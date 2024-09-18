ce1ede614da59ae1d0f0633e7daed594091661a6
Ety <etienne.orio@orio.ch>
2024-09-18 14:12:27 +0200

    First commit

---
+++ test.cpp
@@ -0,0 +1,7 @@
+
+#include "test.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "hello world";
+}
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by Etienne on 18/09/2024.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
